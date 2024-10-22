dcbe4a089e6fdc62dc7e3c5d6e90c893bbaf5aa8
yural64 <yural64@gmail.com>
2024-10-22 22:09:37 +0300

    add get_english_words

--- main.py
+++ main.py
@@ -6,3 +6,29 @@ url = "https://randomword.com/"
 response = requests.get(url)
 
 print(response.text)
+
+#Создаём функцию, которая будет получать информацию
+
+def get_english_words():
+    url = "https://randomword.com/"
+    try:
+        response = requests.get(url)
+        print(response.text)
+
+        #Создаём объект Soup
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        #Получаем слово
+        english_words = soup.find_all("div", id="random_word")
+
+        #Получаем описание слова
+        word_definition = soup.find_all("div", id="random_word_definition")
+
+        # Функция возвращает словарь
+        return {
+            "english_words",
+            "word_definition"
+        }
+    except:
+        print("Произошла ошибка")
+        
