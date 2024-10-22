e51943e9f278120ac64f8ad44781c4432fab35e4
yural64 <yural64@gmail.com>
2024-10-22 22:50:48 +0300

    add word_game

--- main.py
+++ main.py
@@ -31,4 +31,26 @@ def get_english_words():
         }
     except:
         print("Произошла ошибка")
-        
+
+#Создаём функцию, которая будет делать саму игру
+def word_game():
+    print("Добро пожаловать в игру!")
+    while True:
+        word_dict = get_english_words()
+        word = word_dict.get("english_words")
+        word_definition = word_dict.get("word_definition")
+
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово?")
+        if user == word:
+            print("Верно!")
+        else:
+            print(f"Не угадал! Загадано слово {word}")
+
+        play_again = input("Хотите сыграть еще? y/n:")
+        if play_again != "y":
+            print("Игра окончена!")
+            break
+
+#Вызываем игровую функцию
+word_game()
