9759e853666342aacb60038236191676f95595c4
yural64 <yural64@gmail.com>
2024-10-23 12:25:14 +0300

    edit translate

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from googletrans import Translator
 
+#Инициализируем объект переводчика
+translator = Translator()
+
 url = "https://randomword.com/"
 
 response = requests.get(url)
 
 #Создаём функцию, которая будет получать информацию
 
-def get_english_words():
+def get_russian_words():
     url = "https://randomword.com/"
     try:
         response = requests.get(url)
@@ -16,44 +19,32 @@ def get_english_words():
         #Создаём объект Soup
         soup = BeautifulSoup(response.content, "html.parser")
 
-        #Получаем слово
+        #Получаем английское слово
         english_words = soup.find("div", id="random_word").text.strip()
 
-        #Получаем описание слова
+        #Получаем английское описание слова
         word_definition = soup.find("div", id="random_word_definition").text.strip()
 
-        # Функция возвращает словарь
+        #Переводим слово и описание на русский язык
+        translated_word = translator.translate(english_words, dest="ru").text
+        translated_definition = translator.translate(word_definition, dest="ru").text
+
+        # Функция возвращает словарь с переводом слова и его значения
         return {
-            "english_words": english_words,
-            "word_definition": word_definition
-        }
+            "word": translated_word,
+            "word_definition": translated_definition
+            }
     except:
         print("Произошла ошибка")
 
-#Создаем функцию перевода
-def word_translator():
-    translator = Translator()
-    russian = translator.translate("english_words", dest="ru")
-    russian_words = russian.text
-    russian_definition = translator.translate("word_definition", dest="ru")
-#    print("russian_words")
-#    print("russian_definition")
-    return {
-        "russian_words": russian_words,
-        "russian_definition": russian_definition
-    }
 
 #Создаём функцию, которая будет делать саму игру
 def word_game():
     print("Добро пожаловать в игру!")
     while True:
-        word_dict = get_english_words()
-        word = word_dict.get("english_words")
+        word_dict = get_russian_words()
+        word = word_dict.get("word")
         word_definition = word_dict.get("word_definition")
-
-        russ_dict = word_translator()
-        word = russ_dict.get("russian_words")
-        word_definition = russ_dict.get("russian_definition")
 
         print(f"Значение слова - {word_definition}")
         user = input("Что это за слово? ")
